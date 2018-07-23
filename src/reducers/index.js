054f2f2a2ac7c757439c6e6409ba9d3718a6591e
lz <happylezhi@live.com>
2018-07-23 20:27:55 +0800

    add auth.

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -3,8 +3,10 @@ import { combineReducers } from "redux";
 import { routerReducer } from "react-router-redux";
 
 import posts from "./postProducer";
+import auth from "./authRducer";
 
 export default combineReducers({
   posts,
+  auth,
   routerReducer
 });
--- src/types/index.js
+++ src/types/index.js
@@ -1,4 +1,12 @@
-export const ADD_POST = 'ADD_POST';
-export const DELETE_POST = 'DELETE_POST';
-export const UPDATE_POST='UPDATE_POST';
-export const FETCH_POST = 'FETCH_POST';
+export const ADD_POST = "ADD_POST";
+export const DELETE_POST = "DELETE_POST";
+export const UPDATE_POST = "UPDATE_POST";
+export const FETCH_POST = "FETCH_POST";
+
+const authSuccess = () => ({
+  type: "AUTH_SUCCESS"
+});
+
+const authFail = () => ({
+  type: "AUTH_FAIL"
+});
